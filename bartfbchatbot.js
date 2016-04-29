46e124d4699b835f1c241b73ad4562d062a723b2
hoaxoan <nguyendo2008@gmail.com>
2016-04-29 16:50:22 +0700

    Update bartfbchatbot.js

--- bartfbchatbot.js
+++ bartfbchatbot.js
@@ -347,32 +347,31 @@ function processReceipt(sender) {
                         "currency":"USD",
                         "image_url":"http://petersapparel.parseapp.com/img/grayshirt.png"
                       }
-                    ],
-                    "address":{
-                      "street_1":"1 Hacker Way",
-                      "street_2":"",
-                      "city":"Menlo Park",
-                      "postal_code":"94025",
-                      "state":"CA",
-                      "country":"US"
-                    },
-                    "summary":{
-                      "subtotal":75.00,
-                      "shipping_cost":4.95,
-                      "total_tax":6.19,
-                      "total_cost":56.14
-                    },
-                    "adjustments":[
-                      {
-                        "name":"New Customer Discount",
-                        "amount":20
-                      },
-                      {
-                        "name":"$10 Off Coupon",
-                        "amount":10
-                      }
-                    ]
-                }
+                ],
+                "address":{
+                  "street_1":"1 Hacker Way",
+                  "street_2":"",
+                  "city":"Menlo Park",
+                  "postal_code":"94025",
+                  "state":"CA",
+                  "country":"US"
+                },
+                "summary":{
+                  "subtotal":75.00,
+                  "shipping_cost":4.95,
+                  "total_tax":6.19,
+                  "total_cost":56.14
+                },
+                "adjustments":[
+                  {
+                    "name":"New Customer Discount",
+                    "amount":20
+                  },
+                  {
+                    "name":"$10 Off Coupon",
+                    "amount":10
+                  }
+                ]
             }
         }
     };
