ed3bcfac504dcc97ae6839ce7b107fba86e84c5e
hoaxoan <nguyendo2008@gmail.com>
2016-04-29 16:44:05 +0700

    Update bartfbchatbot.js

--- bartfbchatbot.js
+++ bartfbchatbot.js
@@ -261,6 +261,126 @@ function processLocation(sender, coords) {
     });   
 }
 
+function processProduct(sender) {
+    var messageData,
+    messageData = {
+        'attachment': {
+            'type': 'template',
+            'payload': {
+                "template_type":"generic",
+                "elements":[
+                  {
+                    "title":"Classic White T-Shirt",
+                    "image_url":"http://petersapparel.parseapp.com/img/item100-thumb.png",
+                    "subtitle":"Soft white cotton t-shirt is back in style",
+                    "buttons":[
+                      {
+                        "type":"web_url",
+                        "url":"https://petersapparel.parseapp.com/view_item?item_id=100",
+                        "title":"View Item"
+                      },
+                      {
+                        "type":"web_url",
+                        "url":"https://petersapparel.parseapp.com/buy_item?item_id=100",
+                        "title":"Buy Item"
+                      },
+                      {
+                        "type":"postback",
+                        "title":"Bookmark Item",
+                        "payload":"USER_DEFINED_PAYLOAD_FOR_ITEM100"
+                      }              
+                    ]
+                  },
+                  {
+                    "title":"Classic Grey T-Shirt",
+                    "image_url":"http://petersapparel.parseapp.com/img/item101-thumb.png",
+                    "subtitle":"Soft gray cotton t-shirt is back in style",
+                    "buttons":[
+                      {
+                        "type":"web_url",
+                        "url":"https://petersapparel.parseapp.com/view_item?item_id=101",
+                        "title":"View Item"
+                      },
+                      {
+                        "type":"web_url",
+                        "url":"https://petersapparel.parseapp.com/buy_item?item_id=101",
+                        "title":"Buy Item"
+                      },
+                      {
+                        "type":"postback",
+                        "title":"Bookmark Item",
+                        "payload":"USER_DEFINED_PAYLOAD_FOR_ITEM101"
+                      }              
+                    ]
+                  }
+            }
+        }
+    };
+    sendGenericMessage(sender, messageData);
+}
+
+function processReceipt(sender) {
+    var messageData,
+    messageData = {
+        'attachment': {
+            'type': 'template',
+            'payload': {
+                "template_type":"receipt",
+                "recipient_name":"Stephane Crozatier",
+                "order_number":"12345678902",
+                "currency":"USD",
+                "payment_method":"Visa 2345",        
+                "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
+                "timestamp":"1428444852", 
+                "elements":[
+                      {
+                        "title":"Classic White T-Shirt",
+                        "subtitle":"100% Soft and Luxurious Cotton",
+                        "quantity":2,
+                        "price":50,
+                        "currency":"USD",
+                        "image_url":"http://petersapparel.parseapp.com/img/whiteshirt.png"
+                      },
+                      {
+                        "title":"Classic Gray T-Shirt",
+                        "subtitle":"100% Soft and Luxurious Cotton",
+                        "quantity":1,
+                        "price":25,
+                        "currency":"USD",
+                        "image_url":"http://petersapparel.parseapp.com/img/grayshirt.png"
+                      }
+                    ],
+                    "address":{
+                      "street_1":"1 Hacker Way",
+                      "street_2":"",
+                      "city":"Menlo Park",
+                      "postal_code":"94025",
+                      "state":"CA",
+                      "country":"US"
+                    },
+                    "summary":{
+                      "subtotal":75.00,
+                      "shipping_cost":4.95,
+                      "total_tax":6.19,
+                      "total_cost":56.14
+                    },
+                    "adjustments":[
+                      {
+                        "name":"New Customer Discount",
+                        "amount":20
+                      },
+                      {
+                        "name":"$10 Off Coupon",
+                        "amount":10
+                      }
+                    ]
+                }
+            }
+        }
+    };
+    sendGenericMessage(sender, messageData);
+}
+
 function sendTextMessage(sender, text) {
     var messageData = {
         text: text
@@ -344,6 +464,9 @@ app.post('/webhook/', function (req, res) {
             console.log(JSON.stringify(event));
             console.log('^^^^^^^^^^^^^^^^^^^^^^^^');
             sender = event.sender.id;
+            processProduct(sender);
+            processReceipt(sender);
+            
             if (event.message && event.message.attachments && event.message.attachments.length > 0) {
                 attachment = event.message.attachments[0];
 
